49f92257132ea1dd3b468f99537f9bc23f44be4f
PartalogaIrina <7718118@gmail.com>
2023-02-23 19:59:05 +0200

    23.02

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+// var CityName = "Odessa"
+// let cityName = "Kyev"
+// const CITY_NAME = "Dnepro"
+
+
+// let number1 = 5
+// let string = "456456456"
+// let bool = true
+// let array = [1, 2, 3, 4, 5, 55]
+// let cars = {
+//     color: 'red',
+//     type: 'sedan',
+//     Price: '50000'
+// }
+
+// console.log(cars.color);
+
+function alphabetPosition(text) {
+    let result
+    
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    console.log(alphabet.length)
+
+    return result
+}
+
+alphabetPosition("The sunset sets at twelve o' clock")
